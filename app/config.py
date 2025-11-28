edc67a76eea207f3631c08bdc835438eeddb14bd
Amikor <71699670+1AMIKOR1@users.noreply.github.com>
2025-11-29 00:45:19 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,14 +1,18 @@
-from pydantic_settings import BaseSettings
-
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DB_NAME: str
     JWT_ALGORITHM: str
     JWT_SECRET_KEY: str
 
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+    )
+    
     @property
     def get_db_url(self):
-        return f"aiosqlite//{self.DB_NAME}"
+        return f"sqlite+aiosqlite:///{self.DB_NAME}"
 
     @property
     def auth_data(self):
